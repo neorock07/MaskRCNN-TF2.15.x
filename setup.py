2a184ec6a1ed497a5977f93d83a1968b54f12ab3
neorock07 <thepilotrock07@gmail.com>
2024-06-01 20:54:04 +0700

    update setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mrcnn',  # Replace with your desired package name
+    version='0.1.0',  # Replace with your version
+    packages=find_packages(),
+    install_requires=[  # List any external dependencies here
+        'tensorflow',
+        # ... other dependencies
+    ],
+)
